0bf347bde4839ff0be33748d1af237476346c76d
TIAGO-LAPTOP\Tiago <cs.couto@outlook.com>
2017-08-23 22:46:05 -0300

    deteccao de folhas

--- src/main/java/speciesClasssification/Test.java
+++ src/main/java/speciesClasssification/Test.java
@@ -3,9 +3,12 @@ package speciesClasssification;
 /**
  * Created by Tiago on 8/23/2017.
  */
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Test {
@@ -13,6 +16,69 @@ public class Test {
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     public static void main(String[] args) {
-        Mat img = Imgcodecs.imread("path/to/img", Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+        Mat img = Imgcodecs.imread("src/main/data/carambola/carambola 0001.jpg");
+
+        Mat blurredImage = new Mat();
+        Mat hsvImage = new Mat();
+        Mat mask = new Mat();
+        Mat morphOutput = new Mat();
+
+        // remove some noise
+        Imgproc.blur(img, blurredImage, new Size(7, 7));
+
+        // convert the frame to HSV
+        Imgproc.cvtColor(blurredImage, hsvImage, Imgproc.COLOR_BGR2HSV);
+
+        // get thresholding values from the UI
+        // remember: H ranges 0-180, S and V range 0-255
+        Scalar minValues = new Scalar(0, 70,
+                0);
+        Scalar maxValues = new Scalar(90, 255,
+                70);
+
+        // show the current selected HSV range
+        String valuesToPrint = "Hue range: " + minValues.val[0] + "-" + maxValues.val[0]
+                + "\tSaturation range: " + minValues.val[1] + "-" + maxValues.val[1] + "\tValue range: "
+                + minValues.val[2] + "-" + maxValues.val[2];
+
+        // threshold HSV image to select tennis balls
+        Core.inRange(hsvImage, minValues, maxValues, mask);
+
+        // morphological operators
+        // dilate with large element, erode with small ones
+        Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(24, 24));
+        Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(12, 12));
+
+        Imgproc.erode(mask, morphOutput, erodeElement);
+        Imgproc.erode(morphOutput, morphOutput, erodeElement);
+
+        Imgproc.dilate(morphOutput, morphOutput, dilateElement);
+        Imgproc.dilate(morphOutput, morphOutput, dilateElement);
+
+        // find the tennis ball(s) contours and show them
+        img = findAndDrawBalls(morphOutput, img);
+
+        Imgcodecs.imwrite("src/main/data/carambola/carambola 0001-1.jpg", img);
+    }
+    private static Mat findAndDrawBalls(Mat maskedImage, Mat frame)
+    {
+        // init
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+
+        // find contours
+        Imgproc.findContours(maskedImage, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // if any contour exist...
+        if (hierarchy.size().height > 0 && hierarchy.size().width > 0)
+        {
+            // for each contour, display it in blue
+            for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0])
+            {
+                Imgproc.drawContours(frame, contours, idx, new Scalar(255, 255, 0));
+            }
+        }
+
+        return frame;
     }
 }
