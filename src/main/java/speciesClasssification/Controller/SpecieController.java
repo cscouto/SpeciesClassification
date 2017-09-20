a01e4ec72822245567d23a931257aef515b8b9d5
TIAGO-LAPTOP\Tiago <cs.couto@outlook.com>
2017-09-20 11:34:17 -0300

    ajustes cross origin domain

--- src/main/java/speciesClasssification/Controller/SpecieController.java
+++ src/main/java/speciesClasssification/Controller/SpecieController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import speciesClasssification.Model.*;
 import java.io.*;
 
-
+@CrossOrigin
 @RestController
 public class SpecieController {
     @RequestMapping(value = "/getSpecies")
